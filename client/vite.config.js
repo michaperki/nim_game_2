80c89638aafd891a5d21c6a64767593a5aebd2c6
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-02-15 00:44:31 -0600

    tinkering

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'client/dist', // Specify the output directory
-    emptyOutDir: true, // Clear the output directory before building
-    rollupOptions: {
-      input: '/client/src/main.jsx', // Specify the entry point
-    },
-  },
-});
+})
