cca7a204fbc9b599e683294e6478dafab55f1410
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-02-15 01:25:26 -0600

    tnkr

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vite from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default vite.defineConfig({ 
   plugins: [react()],
   server: {
     proxy: {
@@ -13,6 +12,6 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: '../server/public'
+    outDir: '../server/public' // Adjust this path as needed
   }
-})
+});
